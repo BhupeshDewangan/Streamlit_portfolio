dbd7f46c0bc392957bab5bc9a60395f913bc3e69
BhupeshDewangan <bhupeshdewangan2003@gmail.com>
2024-02-01 23:55:12 +0530

    Initial commit

--- certificate.py
+++ certificate.py
@@ -8,17 +8,17 @@ def certificates():
     col1.write("**Certificate 1**")
     col1.image('./Certificates/accenture.jpg')
 
-    col2.write("**Certificate 2**")
+    col2.write("**Certificate 3**")
     col2.image('./Certificates/cognizant.jpg')
 
-    col1.write("**Certificate 3**")
+    col1.write("**Certificate 5**")
     col1.image('./Certificates/excel.png')
 
     # col2
-    col2.write("**Certificate 4**")
+    col2.write("**Certificate 2**")
     col2.image('./Certificates/tata.jpg')
 
-    col1.write("**Certificate 5**")
+    col1.write("**Certificate 4**")
     col1.image('./Certificates/TCSiON.jpg')
 
     # col2.write("Certificate 1")
